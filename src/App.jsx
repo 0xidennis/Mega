c347d6262a65850d4578c331c6d5ce663e5298cb
0xidennis <156088691+0xidennis@users.noreply.github.com>
2025-03-31 10:35:17 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,43 @@ import Home from './Page/Home'
 import ApplicOne from './loanform/ApplicOne'
 import ApplicTwo from './loanform/ApplicTwo'
 import ApplicThre from './loanform/ApplicThre'
-import { BrowserRouter,Routes,Route,} from 'react-router-dom'
+import { BrowserRouter,Routes,Route,useLocation} from 'react-router-dom'
 import LoanService from './Component/LoanService'
 import About from './Component/About'
 import Header from './Static/Header'
 import Footer from './Static/Footer'
+
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  const hiddenRoutes = ['/get-started'];
+
+  const hideHeaderFooter = hiddenRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!hideHeaderFooter && <Header />}
+      {children}
+      {!hideHeaderFooter && <Footer />}
+    </>
+  );
+};
+
 
 const App = () => {
   return (
     <div>
     
       <BrowserRouter>
-      <Header/>
+      <Layout>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/loans" element={<LoanService/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path='/get-started' element={<ApplicOne/>}/>
+        <Route path="/get-started" element={<ApplicOne/>}/>
       </Routes>
-      <Footer/>
+      </Layout>
       </BrowserRouter>
       
       {/* <ApplicTwo/> */}
